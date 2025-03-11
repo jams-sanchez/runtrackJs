068e52d0896020d5533d50bd1b27e1741293d747
James Sanchez <james.sanchez@laplateforme.io>
2025-03-11 10:36:58 +0100

    jour02 job03 ajout de bouton reset compteur

--- jour02/job03/script.js
+++ jour02/job03/script.js
@@ -1,4 +1,4 @@
-
+// 
 let compteur = 0;
 
 const addOne = () => {
@@ -8,3 +8,11 @@ const addOne = () => {
 
 let bouton = document.getElementById("button");
 bouton.addEventListener("click", addOne);
+
+const resetCompteur = () => {
+    compteur = 0;
+    document.getElementById("compteur").innerHTML = compteur;
+}
+
+let resetBut = document.getElementById("reset");
+resetBut.addEventListener("click", resetCompteur);
