883caffbbf7a233d4003da94b2c5372b4dce6ec7
James Sanchez <james.sanchez@laplateforme.io>
2025-03-20 18:23:41 +0100

    add: message d'erreur connexion

--- bigjob/js/script.js
+++ bigjob/js/script.js
@@ -1,52 +1,56 @@
 // récupération des données
 const getJson = async () => {
   try {
-    const reponse = await fetch("./js/data.json");
-    const info = await reponse.text();
-    let fullData = JSON.parse(info);
-    console.log(fullData);
+    const reponse = await fetch("../js/data.json");
+    const fullData = await reponse.json();
     return fullData;
   } catch (error) {
     console.error("Erreur de chargement: ", error);
   }
 };
 
-// empêche les boutons d'actualiser la page
+// variables CONNEXION
 const form = document.querySelector("form");
-form.addEventListener("click", (e) => e.preventDefault());
-
-// CONNEXION
 const btnConnexion = document.getElementById("connexion");
-const inputEmail = document.querySelector('input[type="text"]');
-const inputPass = document.querySelector('input[type="password"]');
-let email = "";
-let password = "";
+const inputEmail = document.getElementById("email");
+const inputPass = document.getElementById("motDePasse");
 
-inputEmail.addEventListener("input", (e) => {
-  email = e.target.value;
-});
-inputPass.addEventListener("input", (e) => {
-  password = e.target.value;
-});
+// empêche les boutons d'actualiser la page
+if (form) {
+  form.addEventListener("click", (e) => e.preventDefault());
+}
 
-const connexion = (email, motDePasse) => {
-  console.log("hello");
+const connexion = () => {
+  getJson().then((data) => {
+    if (data) {
+      let membres = data.find((item) => item.membres)?.membres;
+      membres.forEach((membre) => {
+        const verifMail = membre.email;
+        console.log(verifMail);
+      });
+    }
+  });
 };
 
-btnConnexion.addEventListener("click", (e) => {
-  connexion();
+// connexion();
+
+btnConnexion.addEventListener("click", () => {
+  const email = inputEmail.value.trim();
+  const password = inputPass.value.trim();
+  console.log(email);
+  console.log(password);
+  // affichage msg erreur
+  const errorMsg = document.createElement("p");
+  errorMsg.innerHTML = "Veuillez remplir tous les champs.";
+  errorMsg.style.color = "red";
+  errorMsg.style.fontSize = "1rem";
+  errorMsg.style.fontWeight = "bold";
+
+  if (!email || !password) {
+    console.error("Veuillez remplir tous les champs.");
+    form.append(errorMsg);
+  } else {
+    errorMsg.remove();
+    connexion(email, password);
+  }
 });
-
-// if (data !== undefined) {
-//   // données des élèves
-//   eleves = data.find((item) => item.eleves)?.eleves;
-//   console.log(eleves);
-
-//   // données des modérateurs
-//   mods = data.find((item) => item.moderateurs)?.moderateurs;
-//   console.log(mods);
-
-//   // données des admins
-//   admins = data.find((item) => item.moderateurs)?.moderateurs;
-//   console.log(admins);
-// }
