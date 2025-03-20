815434ce7c4d3fbc9b3b6e26a5e6935e6a70602b
James SANCHEZ <james.sanchez@laplateforme.io>
2025-03-20 21:35:52 +0100

    ajout connexion + verif connexion methodes

--- bigjob/js/script.js
+++ bigjob/js/script.js
@@ -1,3 +1,23 @@
+// affichage msg info / error
+const infoMsg = document.createElement("p");
+infoMsg.innerHTML = "Veuillez remplir tous les champs.";
+infoMsg.style.color = "red";
+infoMsg.style.fontSize = "1rem";
+infoMsg.style.fontWeight = "bold";
+
+// variables CONNEXION
+const form = document.querySelector("form");
+const btnConnexion = document.getElementById("connexion");
+const inputEmail = document.getElementById("email");
+const inputPass = document.getElementById("motDePasse");
+let email;
+let password;
+
+// empêche les boutons d'actualiser la page
+if (form) {
+  form.addEventListener("click", (e) => e.preventDefault());
+}
+
 // récupération des données
 const getJson = async () => {
   try {
@@ -9,48 +29,62 @@ const getJson = async () => {
   }
 };
 
-// variables CONNEXION
-const form = document.querySelector("form");
-const btnConnexion = document.getElementById("connexion");
-const inputEmail = document.getElementById("email");
-const inputPass = document.getElementById("motDePasse");
-
-// empêche les boutons d'actualiser la page
-if (form) {
-  form.addEventListener("click", (e) => e.preventDefault());
-}
-
-const connexion = () => {
+// vérifie le mail et mot de passe
+const verifInfo = () => {
   getJson().then((data) => {
     if (data) {
       let membres = data.find((item) => item.membres)?.membres;
+      let mailTrouve = false;
+      let passTrouve = false;
+
       membres.forEach((membre) => {
-        const verifMail = membre.email;
-        console.log(verifMail);
+        if (membre.email === email) {
+          mailTrouve = true;
+          if (password === membre.motDePasse) {
+            passTrouve = true;
+            membre.connecte = true;
+            infoMsg.innerText = ` Vous êtes connecté en tant que ${membre.role}`;
+            form.append(infoMsg);
+            setTimeout(() => {
+              infoMsg.remove();
+              location.href = "../pages/calendrier.html";
+            }, 2000);
+          } else {
+            passTrouve = false;
+            infoMsg.innerText = "Mot de passe incorrect";
+            form.append(infoMsg);
+            if (infoMsg) {
+              inputPass.addEventListener("focus", () => infoMsg.remove());
+            }
+          }
+        } else if (!mailTrouve) {
+          infoMsg.innerText = "Email incorrect";
+          form.append(infoMsg);
+          if (infoMsg) {
+            inputEmail.addEventListener("focus", () => infoMsg.remove());
+          }
+        }
       });
     }
   });
 };
 
-// connexion();
+// déclenche la connexion
+if (btnConnexion) {
+  btnConnexion.addEventListener("click", () => {
+    email = inputEmail.value.trim();
+    password = inputPass.value.trim();
 
-btnConnexion.addEventListener("click", () => {
-  const email = inputEmail.value.trim();
-  const password = inputPass.value.trim();
-  console.log(email);
-  console.log(password);
-  // affichage msg erreur
-  const errorMsg = document.createElement("p");
-  errorMsg.innerHTML = "Veuillez remplir tous les champs.";
-  errorMsg.style.color = "red";
-  errorMsg.style.fontSize = "1rem";
-  errorMsg.style.fontWeight = "bold";
-
-  if (!email || !password) {
-    console.error("Veuillez remplir tous les champs.");
-    form.append(errorMsg);
-  } else {
-    errorMsg.remove();
-    connexion(email, password);
-  }
-});
+    if (!email || !password) {
+      console.error("Veuillez remplir tous les champs.");
+      form.append(infoMsg);
+      if (infoMsg) {
+        inputEmail.addEventListener("focus", () => infoMsg.remove());
+        inputPass.addEventListener("focus", () => infoMsg.remove());
+      }
+    } else {
+      infoMsg.remove();
+      verifInfo(email, password);
+    }
+  });
+}
