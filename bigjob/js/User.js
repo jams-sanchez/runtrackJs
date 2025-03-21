d27bf7af96fa8889c29f2eeee315b341016ea9c2
James SANCHEZ <james.sanchez@laplateforme.io>
2025-03-21 18:04:20 +0100

    fix connexion + ajout verif mail methode pour l'inscription

--- bigjob/js/User.js
+++ bigjob/js/User.js
@@ -1,37 +1,34 @@
-class Eleve {
+class Membre {
   constructor(email, motDePasse) {
-    this._email = email;
-    this._motDePasse = motDePasse;
-    this._role = "eleve";
-    this._connecte = false;
-    this._presence = [];
+    this.id = users.length + 1;
+    this.email = email;
+    this.motDePasse = motDePasse;
+    this.role = "eleve";
+    this.connecte = false;
   }
 
   // recupere les variables
-  get email() {
-    return this._email;
-  }
-  get motDePasse() {
-    return this._motDePasse;
-  }
-  get role() {
-    return this._role;
-  }
-  get connecte() {
-    return this._connecte;
-  }
-  get presence() {
-    return this._presence;
-  }
+  // get email() {
+  //   return this._email;
+  // }
+  // get motDePasse() {
+  //   return this._motDePasse;
+  // }
+  // get role() {
+  //   return this._role;
+  // }
+  // get connecte() {
+  //   return this._connecte;
+  // }
 
   // modif des variables
-  set role(newRole) {
-    return (this._role = newRole);
-  }
-  set connecte(login) {
-    return (this._connecte = login);
-  }
-  set presence(newDate) {
-    return this._presence.push(newDate);
-  }
+  // set role(newRole) {
+  //   return (this._role = newRole);
+  // }
+  // set connecte(login) {
+  //   return (this._connecte = login);
+  // }
+  // set presence(newDate) {
+  //   return this._presence.push(newDate);
+  // }
 }
--- bigjob/js/script.js
+++ bigjob/js/script.js
@@ -1,5 +1,13 @@
-let fausseBDD = JSON.parse(sessionStorage.getItem("infos"));
-console.log(fausseBDD);
+let users = JSON.parse(sessionStorage.getItem("users"));
+// console.log(users);
+
+//
+// pour creer nouveau membre //
+// let signup = new Membre("bla@laplateforme.io", "test");
+// users.push(signup);
+// console.log(users);
+// sessionStorage.setItem("users", JSON.stringify(users));
+//
 
 // MESSAGE: style message d'info ou d'erreur
 const infoMsg = document.createElement("p");
@@ -43,56 +51,53 @@ if (role && role === "moderateur") {
 
 // CONNEXION: vérifie le mail et mot de passe
 const verifInfo = (mail, pass) => {
-  getJson().then((data) => {
-    if (data) {
-      let membres = data.find((item) => item.membres)?.membres;
-      let mailTrouve = false;
-      let passTrouve = false;
+  if (users) {
+    let mailTrouve = false;
+    let passTrouve = false;
 
-      membres.forEach((membre) => {
-        if (membre.email === mail) {
-          mailTrouve = true;
-          if (membre.motDePasse === pass) {
-            passTrouve = true;
-            membre.connecte = true;
-            infoMsg.innerText = "Vous êtes connecté";
-            form.append(infoMsg);
+    users.forEach((membre) => {
+      if (membre.email === mail) {
+        mailTrouve = true;
+        if (membre.motDePasse === pass) {
+          passTrouve = true;
+          membre.connecte = true;
+          infoMsg.innerText = "Vous êtes connecté";
+          form.append(infoMsg);
 
-            // stockage des infos user connecté dans une session
-            sessionStorage.setItem("email", mail);
-            sessionStorage.setItem("role", membre.role);
-            let creeNom = mail.replace("@", " ");
-            creeNom = creeNom.split(" ");
-            creeNom = creeNom[0];
-            sessionStorage.setItem("nom", creeNom);
+          // stockage des infos user connecté dans une session
+          sessionStorage.setItem("email", mail);
+          sessionStorage.setItem("role", membre.role);
+          let creeNom = mail.replace("@", " ");
+          creeNom = creeNom.split(" ");
+          creeNom = creeNom[0];
+          sessionStorage.setItem("nom", creeNom);
 
-            // redirection vers page selon role
-            setTimeout(() => {
-              infoMsg.remove();
-              if (membre.role === "eleve") {
-                location.href = "../pages/calendrier.html";
-              } else {
-                location.href = "../pages/backoffice.html";
-              }
-            }, 2000);
-          } else {
-            passTrouve = false;
-            infoMsg.innerText = "Mot de passe incorrect";
-            form.append(infoMsg);
-            if (infoMsg) {
-              inputPass.addEventListener("focus", () => infoMsg.remove());
+          // redirection vers page selon role
+          setTimeout(() => {
+            infoMsg.remove();
+            if (membre.role === "eleve") {
+              location.href = "../pages/calendrier.html";
+            } else {
+              location.href = "../pages/backoffice.html";
             }
-          }
-        } else if (!mailTrouve) {
-          infoMsg.innerText = "Email incorrect";
+          }, 2000);
+        } else {
+          passTrouve = false;
+          infoMsg.innerText = "Mot de passe incorrect";
           form.append(infoMsg);
           if (infoMsg) {
-            inputEmail.addEventListener("focus", () => infoMsg.remove());
+            inputPass.addEventListener("focus", () => infoMsg.remove());
           }
         }
-      });
-    }
-  });
+      } else if (!mailTrouve) {
+        infoMsg.innerText = "Email incorrect";
+        form.append(infoMsg);
+        if (infoMsg) {
+          inputEmail.addEventListener("focus", () => infoMsg.remove());
+        }
+      }
+    });
+  }
 };
 
 // CONNEXION: déclenche la connexion
@@ -118,17 +123,17 @@ if (btnConnexion) {
 
 // verif email
 function looksLikeMail(mail) {
-  const lastAtPos = mail.lastIndexOf("@");
-  const laPlat = "@laplateforme.io";
+  const lastAt = mail.lastIndexOf("@");
+  const beforeAt = mail.slice(0, lastAt); // texte avant @
+  const afterAt = mail.slice(lastAt + 1); // texte apres @
+  const laPlat = "laplateforme.io";
+  const regex = /^[A-Za-z0-9]+$/; // lettres maj ou min ou chiffres
 
-  // return (
-  //   lastAtPos < lastDotPos &&
-  //   lastAtPos > 0 &&
-  //   mail.indexOf("@@") == -1 &&
-  //   lastDotPos > 2 &&
-  //   mail.length - lastDotPos > 2
-  // );
+  // console.log(lastAt > 3 && afterAt === laPlat && regex.test(beforeAt));
+  return lastAt > 3 && afterAt === laPlat && regex.test(beforeAt);
 }
+
+looksLikeMail("testi@laplat@eforme.io");
 
 // lancement de l'inscription
 if (btnInscription) {
